b2110159bcabbfc227327e537af43c59af366ee4
behnam <beh.banikarimi@gmail.com>
2023-07-21 11:27:06 +1000

    add sql and database

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from sqlalchemy import text
+import os 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'taskmanager.db')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    password= db.Column(db.Integer)
+    
+    
+    def __init__(self, id, email, password,):
+        self.id = id
+        self.email = email
+        self.total_price = password
+        
+with app.app_context():
+    try:
+        db.session.execute(text('SELECT * from users'))
+        print("Database connection successful!")
+    except Exception as e:
+        print("Failed to connect to the database:", str(e))        
 
 CORS(app)
 
@@ -32,7 +58,11 @@ tasks = [
 def get_tasks():
     return jsonify(tasks)
 
-
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    json_data = request.get_json()
+    print(json_data)
+    return make_response ('success')
 
 if __name__ == '__main__':
     app.debug = True
